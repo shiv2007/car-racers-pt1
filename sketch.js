6406f1a31c79f5765d9de6301751ce9e9b3b42a8
shiv2007 <shivgupta10112007@gmail.com>
2020-04-24 21:04:01 +0530

    ball

--- sketch.js
+++ sketch.js
@@ -1,13 +1,19 @@
-var ball;
+var  hypnoticBall, database; 
+var position
 
 function setup(){
+    database = firebase.database();
+    console.log(database);
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+     hypnoticBall = createSprite(250,250,10,10);
+     hypnoticBall.shapeColor = "red";
+    var hypnoticBallPosition = database.ref('ball/position')
+    hypnoticBallPosition.on("value", readPositoin,showError)
 }
 
 function draw(){
     background("white");
+    
     if(keyDown(LEFT_ARROW)){
         changePosition(-1,0);
     }
@@ -23,7 +29,23 @@ function draw(){
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
-}
+
+      
+
+function writePosition(x,y){
+    database.ref('ball/position').set({
+      'x': position.x + x ,
+      'y': position.y + y
+    })
+  }
+  
+  function readPosition(data){
+    position = data.val();
+    console.log(position.x);
+    hypnoticBall.x = position.x;
+    hypnoticBall.y = position.y;
+  }
+  
+  function showError(){
+    console.log("Error in writing to the database");
+  }
